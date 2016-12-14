7cb0b1ebea3aa3d5ffc56268f5e52fb790d5b511
Rene Richter <richterrettich@gmail.com>
2016-12-14 21:09:42 +0100

    refac: rework kvm

---
+++ kvm/shell/create_base_image.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+./create_disk.sh
+
+virt-install \
+    --name demo2 \
+    --memory 4000 \
+    --vcpus=2,maxvcpus=4 \
+    --disk ../out/shell/test.img,size=4,format=qcow2,device=disk,bus=virtio \
+    --network bridge=bridge0 \
+    --cpu host \
+    --virt-type kvm \
+    --os-type=linux \
+    --os-variant generic  \
+    --graphics vnc,password=foobar,port=5910,keymap=de \
+    --cd $HOME/Downloads/archlinux-2016.12.01-dual.iso
--- kvm/shell/create_vm.sh
+++ kvm/shell/create_vm.sh
@@ -2,17 +2,17 @@
 
 
 virt-install \
-  --name demo \
+  --name demo2 \
   --memory 4000 \
   --vcpus=2,maxvcpus=4 \
-  --disk ./test.img,size=4,format=qcow2,device=disk,bus=virtio \
+  --disk ./test2.img,size=4,format=qcow2,device=disk,bus=virtio \
   --network bridge=bridge0 \
   --cpu host \
   --virt-type kvm \
   --os-type=linux \
   --os-variant centos7.0  \
-  --cd $HOME/Downloads/archlinux-2016.12.01-dual.iso
+  --graphics vnc,password=foobar,port=5910,keymap=de 
+#  --cd $HOME/Downloads/ubuntu-16.04.1-server-amd64.iso \ 
 #  --location 'http://mirror.i3d.net/pub/centos/7/os/x86_64/' \
-  --console pty,target_type=serial \
-  --graphics vnc,password=foobar,port=5910,keymap=de \
-  --extra-args 'console=ttyS0,115200n8 serial'
+#  --console pty,target_type=serial \
+#  --extra-args 'console=ttyS0,115200n8 serial'
